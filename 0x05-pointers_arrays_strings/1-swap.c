630bf4e0da8bb8408a9b4b90379a1567ed9cc663
Vamprica <oluwracheal@gmail.com>
2022-07-13 15:54:22 -0700

    c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main 0- Entry point
- *
- * Return: n
+ * swap_int - a function that swaps the values of two integers 
+ * @a: input 1
+ * @b: input 2 
+ * Return: integers
  */
 void swap_int(int *a, int *b)
 {
