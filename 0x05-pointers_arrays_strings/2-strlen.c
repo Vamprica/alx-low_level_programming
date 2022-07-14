a185346f256994a0a870c571feca57ee53dc67a3
Vamprica <oluwracheal@gmail.com>
2022-07-14 15:24:59 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * swap_int - a function that swaps the values of two integers
- * @a: input 1
- * @b: input 2
- * Return: integers
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
  */
 int _strlen(char *s)
 {
